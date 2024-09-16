7cb1004a27e3dfeb545157ef007be77d2e1f904d
Yanzz231 <mrpartys05@gmail.com>
2024-09-16 20:44:11 +0700

    Add Note

--- models/users.js
+++ models/users.js
@@ -10,10 +10,15 @@ const {responseJson} = require("../helper/response");
 
 const Users = {
     login: async (res, data) => {
+        // DATA BODY
         const {username, email, password} = data
+
+        // TEMPORARY DATA
         var side_data = []
         var account = ""
+
         try {
+            // CHECKING EMAIL AND USERNAME
             const [email_check] = await db.promise().execute("SELECT * FROM users WHERE email = ?", [email])
             if (email_check.length === 0) {
                 const [username_check] = await db.promise().execute("SELECT * FROM users WHERE username = ?", [username])
@@ -28,16 +33,21 @@ const Users = {
                 account = email
             }
 
+            // CHECKING PASSWORD WITH BCRYPT COMPARE
             const checkPassword = await bcrypt.compare(password, side_data[0].password)
             if (!checkPassword) {
                 return responseJson(res, "password_incorrect", [], "Password Salah")
             }
 
+            // CONVERST TOKEN WITH JWT
             const token = jwt.sign({}, process.env.JWT_SECRET)
 
+            // SQL UPDATE TOKEN TO JWT CODE
             const sqlMessage = 'UPDATE users SET token = ? WHERE email = ?';
             await db.promise().execute(sqlMessage, [token, account])
+
             return responseJson(res, true, {account: account, password: password, token: token}, "Berhasil Login")
+
         } catch (err) {
             return responseJson(res, false, [], err.message)
         }
@@ -45,29 +55,41 @@ const Users = {
 
 
     create: async (res, data) => {
+        // DATA BODY
         const {username, email, password} = data
+
         try {
+            // CHECKING EMAIL
             const [email_check] = await db.promise().execute('SELECT * FROM users WHERE email = ?', [email])
             if (email_check.length > 0) {
                 return responseJson(res, false, [], "Email Sudah Terdaftar")
             }
 
+            // CONVERT PASSWORD TO BCRYPT CODE
             const hashPassword = await bcrypt.hash(password, 10)
+            // DEFAULT TOKEN IS NULL
             const token = null
 
+            // INSERT DATA BY DATA BODY
             const sqlMessage = 'INSERT INTO users (username, email, password, token) VALUES(?, ?, ?, ?)'
             await db.promise().execute(sqlMessage, [username, email, hashPassword, token])
 
             return responseJson(res, true, data, "Berhasil Menambahkan Data Users")
+
         } catch (err) {
             return responseJson(res, false, [], err.message)
         }
     },
 
     logout: async (res, data) => {
+        // DATA BODY
         const {username, email, password} = data
+
+        // TEMPORARY DATA
         var account = ""
+
         try {
+            // CHECKING EMAIL AND USERNAME
             const [email_check] = await db.promise().execute("SELECT * FROM users WHERE email = ?", [email])
             if (email_check.length === 0) {
                 const [username_check] = await db.promise().execute("SELECT * FROM users WHERE username = ?", [username])
@@ -80,9 +102,12 @@ const Users = {
                 account = email
             }
 
+            // SQL UPDATE TOKEN TO NULL
             const sqlMessage = 'UPDATE users SET token = ? WHERE email = ?';
             await db.promise().execute(sqlMessage, [null, account])
+
             return responseJson(res, true, {account: account, password: password}, "Berhasil Logout")
+
         } catch (err) {
             return responseJson(res, false, [], err.message)
         }
