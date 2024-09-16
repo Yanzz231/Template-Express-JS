20ff3e7e272ad35f16c6a17a36965376df8d9af8
Yanzz231 <mrpartys05@gmail.com>
2024-09-16 19:10:38 +0700

    master

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var chalk = require('chalk');
 
-var indexRouter = require('./routes/index');
+require('dotenv').config();
+
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.listen(process.env.PORT)
+
+const port = process.env.port || 3000
+app.listen(port, () => {
+    console.log(chalk.green(`Listen to Port ${port}`));
+})
 
 module.exports = app;
